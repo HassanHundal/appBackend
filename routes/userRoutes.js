d102f5974a6f61f680eadffaaf564d47db25cc9e
m-atique <atiquenwl@gmail.com>
2023-09-14 17:11:52 +0500

    user apis

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,89 @@
-const express = require('express');
-const db = require('../config/db')
+const express = require("express");
+const db = require("../config/db");
 const router = express.Router();
 
+router.get("/getUser/:userCnic", async (req, res) => {
+  const user = req.params.userCnic;
+  const result = await db.query(
+    `select userCnic,userPwd from users where userCnic = ${user}`
+  );
+  if (result) {
+    res.status(200).json(result.recordset);
+  }
+});
 
-router.get('/getUser/:userCnic',async(req,res)=>{
-   const user = req.params.userCnic
-    const result = await db.query(`select userCnic,userPwd from users where userCnic = ${user}` )
-    if(result){
+//==================addd user to database
 
-        res.status(200).json(result.recordset)
+router.post("/addUser", (req, res) => {
+  const data = req.body;
+
+  // SQL query to insert the data into the database
+  const qry = `INSERT INTO users (
+        userCnic,
+        userName,
+        userPwd,
+        cellNo,
+        rank,
+        beltNo,
+        role,
+        status,
+        beatId,
+        sectorId,
+        zoneId
+              ) VALUES (
+
+                '${data.userCnic}',
+                '${data.userName}',
+                '${data.userPwd}',
+                '${data.cellNo}',
+                '${data.rank}',
+                '${data.beltNo}',
+                '${data.role}',
+                '${data.status}',
+                '${data.beatId}',
+                '${data.sectorId}',
+                '${data.zoneId}'
+                
+)`;
+
+  db.query(qry, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500); // Internal Server Error
+    } else {
+      res.sendStatus(200); // OK
     }
-} );
+  });
+});
 
-module.exports = router
+//==================Update user to database
 
+router.patch("/updateUser/:cnic", (req, res) => {
+  const cnic = req.params.cnic;
+  const data = req.body;
+  // SQL query to update the data into the database
+  const qry = `UPDATE users set 
+  userName = '${data.userName}',
+  userPwd = '${data.userPwd}',
+  cellNo = '${data.cellNo}',
+  rank = '${data.rank}',
+  beltNo = '${data.beltNo}',
+  role = '${data.role}',
+  status = '${data.status}',
+  beatId = '${data.beatId}',
+  sectorId = '${data.sectorId}',
+  zoneId = '${data.zoneId}'
 
+  where userCnic =  '${cnic}'`;
 
+  db.query(qry, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500); // Internal Server Error
+    } else {
+      res.sendStatus(200); // OK
+    }
+  });
+});
+
+module.exports = router;
