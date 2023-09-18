48ac83e5b945e13dec02d8c53a25b2b8f7b789c5
m-atique <atiquenwl@gmail.com>
2023-09-18 16:47:32 +0500

    ADDPSV API CODED

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,27 +1,76 @@
-const express = require('express');
-const db = require('../config/db')
+const express = require("express");
+const db = require("../config/db");
 const router = express.Router();
 
 //get specific driver based on cnic
-router.get('/getPsv/:pre/:no/:model',async(req,res)=>{
-   const psvPre = req.params.pre
-   const psvNo= req.params.no
-   const psvModel = req.params.model
-  
- 
-    const result = await db.query(`select * from  VehicleInfo  where PrefixRegNo = '${psvPre}' and RegNo = '${psvNo}' and VehicleModel = ${psvModel} `)
+router.get("/getPsv/:pre/:no/:model", async (req, res) => {
+  const psvPre = req.params.pre;
+  const psvNo = req.params.no;
+  const psvModel = req.params.model;
 
-    if(result){
+  const result = await db.query(
+    `select * from  psvVehicles  where prefixRegNo = '${psvPre}' and regNo = '${psvNo}' and vehicleModel = ${psvModel} `
+  );
 
-        res.status(200).json(result.recordset)
+  if (result) {
+    res.status(200).json(result.recordset);
+  }
+});
+//------------------------//==================addd driver to database
+
+router.post("/addPsv", (req, res) => {
+  const data = req.body;
+
+  // SQL query to insert the data into the database
+  const qry = `insert into psvVehicles (
+        vehicleType,
+        prefixRegNo,
+        vehicleModel,
+        regNo,
+        chasisNo,
+        engineNo,
+        vehicleMake,
+        vehicleColor,
+        acStatus,
+        seatingCap,
+        trackerStatus,
+        exitGate,
+        manufactureYear,
+        companyName,
+        formOneStatus
+        )
+        values (
+                '${data.vehicleType}',
+                '${data.prefixRegNo}',
+                '${data.vehicleModel}',
+                '${data.regNo}',
+                '${data.chasisNo}',
+                '${data.engineNo}',
+                '${data.vehicleMake}',
+                '${data.vehicleColor}',
+                '${data.acStatus}',
+                '${data.seatingCap}',
+                '${data.trackerStatus}',
+                '${data.exitGate}',
+                '${data.manufactureYear}',
+                '${data.companyName}',
+                '${data.formOneStatus}'
+)` 
+db.query(qry, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500); // Internal Server Error
+    } else {
+      console.log("Data inserted successfully");
+      res.sendStatus(200); // OK
     }
-} );
+  });
+});
+
+//------------------------------add documents 
 
 
 
-
-
-module.exports = router
-
+module.exports = router;
 
 
