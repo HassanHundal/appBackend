1cc0bfa65c7b4652039735885736512fa08a3624
m-atique <atiquenwl@gmail.com>
2023-09-26 09:43:17 +0500

    inspectionroutes updated

--- routes/inspectionRoutes.js
+++ routes/inspectionRoutes.js
@@ -3,15 +3,16 @@ const db = require("../config/db");
 const router = express.Router();
 
 //get specific inspecteion report of specific PSV 
-router.get("/inspectPsv/:pre/:model/:no/:dvrCnic", async (req, res) => {
+router.get("/inspectPsv/:pre/:model/:no/:dvrCnic/:location", async (req, res) => {
   const psvPre = req.params.pre;
   const psvModel = req.params.model;
   const psvNo = req.params.no;
   const dvrCnic = req.params.dvrCnic;
+  const location = req.params.location;
 
   const result = await db.query(
-    `select routeExpiryDate, iIF(GETDATE()>routeExpiryDate,'Expired','Valid') as routeValidity,routeFrom,routeTo, fitnessExpiryDate, iIF(GETDATE()>fitnessExpiryDate,'Expired','Valid') as fitnessValidity,tyreCondition,tyreExpiry, iIF(GETDATE()>tyreExpiry,'Expired','Valid')as tyreValidity, iIF(trackerStatus=0,'Not Installed','Installed')as trackerStatus,iIF(exitGate=0,'Not Installed','Installed')as exitGate,iIF(fireExt=0,'Not Avaible','Availble')as fireExt,iIF(GETDATE()>fireExpiry,'Expired','Valid')as fireValidity,fireExpiry,iIF(regPlates=0,'As per Pattern','Out of pattern')as numPlate, seatingCap,psvDriver.driverName,psvDriver.licenseNo,psvDriver.licenseType, psvDriver.licenseExpiry,iIF(GETDATE()>psvDriver.licenseExpiry,'Expired','Valid')as licenseValidity from psvVehicles,psvDriver where psvNo = '${psvPre+psvModel+psvNo}' and psvDriver.cnic = '${dvrCnic}'`
-  );
+    `SELECT * FROM (select CONCAT_WS('-',prefixRegNo,vehicleModel,regNo)as psvNo, routeExpiryDate,companyName, iIF(GETDATE()>routeExpiryDate,'Expired','Valid') as routeValidity,routeFrom,routeTo, fitnessExpiryDate, iIF(GETDATE()>fitnessExpiryDate,'Expired','Valid') as fitnessValidity,tyreCondition,tyreExpiry, iIF(GETDATE()>tyreExpiry,'Expired','Valid')as tyreValidity, iIF(trackerStatus=0,'Not Installed','Installed')as trackerStatus,iIF(exitGate=0,'Not Installed','Installed')as exitGate,iIF(fireExt=0,'Not Avaible','Availble')as fireExt,iIF(GETDATE()>fireExpiry,'Expired','Valid')as fireValidity,fireExpiry,iIF(regPlates=0,'As per Pattern','Out of pattern')as numPlate, seatingCap,psvDriver.driverName,psvDriver.licenseNo,psvDriver.licenseType, psvDriver.licenseExpiry,iIF(GETDATE()>psvDriver.licenseExpiry,'Expired','Valid')as licenseValidity from psvVehicles,psvDriver where psvNo = '${psvPre+psvModel+psvNo}' and psvDriver.cnic = '${dvrCnic}') AS REPORT,
+    (select count(psvNo) as tripcount from inspectionReport where psvNo = '${psvPre +"-"+psvModel+"-"+psvNo}' and addedDate >= GETDATE()-1 and chkPoint = '${location}') as tripcount`);
 
   if (result) {
     res.status(200).json(result.recordset);
@@ -25,8 +26,9 @@ router.post("/addInspection", (req, res) => {
     const data = req.body;
    
     const qry = `insert into inspectionReport (
-        reportId,
-        psvNo,	
+       
+        psvNo,
+        companyName,	
         routeStatus,
         routePath,
         fitnessStatus,
@@ -48,8 +50,9 @@ router.post("/addInspection", (req, res) => {
         chkPoint
           )
           values (
-            '${data.reportId}',
+            
             '${data.psvNo}',
+            '${data.companyName}',
             '${data.routeStatus}',
             '${data.routePath}',
             '${data.fitnessStatus}',
